d1095a19c45348becc8e10e9e5747b5a5c94c886
remotedev21 <89644241+remotedev21@users.noreply.github.com>
2021-09-05 17:18:29 +0100

    ui-changes

--- src/pages/FaaS/index.tsx
+++ src/pages/FaaS/index.tsx
@@ -36,9 +36,9 @@ const Button = styled.button`
   cursor: pointer;
   border-radius: 16px;
   border: none;
-  background-color: lightgray;
+  background-color: #64ffda;
   &:hover {
-    background-color: gray;
+    background-color: #ccd6f6;
   }
   font-size: 18px;
 `
@@ -83,39 +83,50 @@ const Buttons = styled.div`
   padding-top: 30px;
 `
 
-const ApproveCreateButtons =  ({ tokenA, tokenB, rewardTokenAddress, rewardTokenAmount, stakingDuration, startTime }) => {
-  const { isApproved, isCreated, createPool, approve } = usePoolCreator({ tokenA, tokenB, rewardTokenAddress, rewardTokenAmount, stakingDuration, startTime })
+const ApproveCreateButtons = ({
+  tokenA,
+  tokenB,
+  rewardTokenAddress,
+  rewardTokenAmount,
+  stakingDuration,
+  startTime,
+}) => {
+  const { isApproved, isCreated, createPool, approve } = usePoolCreator({
+    tokenA,
+    tokenB,
+    rewardTokenAddress,
+    rewardTokenAmount,
+    stakingDuration,
+    startTime,
+  })
   return (
-      <Buttons>
-        
-        {!isApproved && (
-          <>
-            <Button onClick={approve}>Approve</Button>
-            <Row>
-              <SmallText>Step 2 / 3</SmallText>
-            </Row>
-          </>
-        )}
-        {isApproved && !isCreated && (
-          <>
-            <Button onClick={createPool}>Create Pool</Button>
-            <Row>
-              <SmallText>Step 3 / 3</SmallText>
-            </Row>
-          </>
-        )}
-        {isCreated && (
-          <>
-            <Text>Success!</Text>
-          </>
-        )}
-      </Buttons>
+    <Buttons>
+      {!isApproved && (
+        <>
+          <Button onClick={approve}>Approve</Button>
+          <Row>
+            <SmallText>Step 2 / 3</SmallText>
+          </Row>
+        </>
+      )}
+      {isApproved && !isCreated && (
+        <>
+          <Button onClick={createPool}>Create Pool</Button>
+          <Row>
+            <SmallText>Step 3 / 3</SmallText>
+          </Row>
+        </>
+      )}
+      {isCreated && (
+        <>
+          <Text>Success!</Text>
+        </>
+      )}
+    </Buttons>
   )
 }
 
-
 export default function Pool() {
-
   const [tokenA, setTokenA] = useState('')
   const [tokenB, setTokenB] = useState('')
   const [rewardTokenAddress, setRewardTokenAddress] = useState('')
@@ -134,27 +145,57 @@ export default function Pool() {
         <Inputs>
           <FlexDiv>
             <Text>Token A:</Text>
-            <Input placeholder='Token A address' type='text' value={tokenA} onChange={(e) => setTokenA(e.target.value)}/>
+            <Input
+              placeholder="Token A address"
+              type="text"
+              value={tokenA}
+              onChange={(e) => setTokenA(e.target.value)}
+            />
           </FlexDiv>
           <FlexDiv>
             <Text>Token B:</Text>
-            <Input placeholder='Token B address' type='text' value={tokenB} onChange={(e) => setTokenB(e.target.value)}/>
+            <Input
+              placeholder="Token B address"
+              type="text"
+              value={tokenB}
+              onChange={(e) => setTokenB(e.target.value)}
+            />
           </FlexDiv>
           <FlexDiv>
             <Text>Reward token:</Text>
-            <Input placeholder='Reward token address' type='text' value={rewardTokenAddress} onChange={(e) => setRewardTokenAddress(e.target.value)}/>
+            <Input
+              placeholder="Reward token address"
+              type="text"
+              value={rewardTokenAddress}
+              onChange={(e) => setRewardTokenAddress(e.target.value)}
+            />
           </FlexDiv>
           <FlexDiv>
             <Text>Reward amount:</Text>
-            <Input placeholder='Reward amount' type='text' value={rewardTokenAmount} onChange={(e) => setRewardTokenAmount(e.target.value)}/>
+            <Input
+              placeholder="Reward amount"
+              type="text"
+              value={rewardTokenAmount}
+              onChange={(e) => setRewardTokenAmount(e.target.value)}
+            />
           </FlexDiv>
           <FlexDiv>
             <Text>Staking duration:</Text>
-            <Input placeholder='Staking duration in sec' type='text' value={stakingDuration} onChange={(e) => setStakingDuration(e.target.value)}/>
+            <Input
+              placeholder="Staking duration in sec"
+              type="text"
+              value={stakingDuration}
+              onChange={(e) => setStakingDuration(e.target.value)}
+            />
           </FlexDiv>
           <FlexDiv>
             <Text>Start time:</Text>
-            <Input placeholder='Start time in sec' type='text' value={startTime} onChange={(e) => setStartTime(e.target.value)}/>
+            <Input
+              placeholder="Start time in sec"
+              type="text"
+              value={startTime}
+              onChange={(e) => setStartTime(e.target.value)}
+            />
           </FlexDiv>
           <Row>
             <Button onClick={() => setPoolInfoReady(true)}>Create Farm</Button>
@@ -165,26 +206,21 @@ export default function Pool() {
         </Inputs>
       )}
       {poolInfoReady && (
-        <ApproveCreateButtons 
+        <ApproveCreateButtons
           tokenA={tokenA}
           tokenB={tokenB}
-          rewardTokenAddress={rewardTokenAddress} 
+          rewardTokenAddress={rewardTokenAddress}
           rewardTokenAmount={rewardTokenAmount}
           stakingDuration={parseInt(stakingDuration)}
-          startTime={parseInt(startTime)}  
+          startTime={parseInt(startTime)}
           // tokenA='0xdF5f5b7aA8546Ec6C480f6Ac1D684E317151d400'
           // tokenB='0xDC573Aa094D9c7B10f0c70fb4060844a8Abab872'
-          // rewardTokenAddress='0xdF5f5b7aA8546Ec6C480f6Ac1D684E317151d400' 
+          // rewardTokenAddress='0xdF5f5b7aA8546Ec6C480f6Ac1D684E317151d400'
           // rewardTokenAmount='24'
           // stakingDuration={86400}
-          // startTime={1616716481}  
+          // startTime={1616716481}
         />
       )}
     </>
   )
 }
-
-
-
-
-
